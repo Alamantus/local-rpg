f96e77ad50fc373d7983a4f60bc276bddc2eab2a
Robbie Antenesse <dev@alamantus.com>
2017-10-11 11:11:50 -0600

    Split out electron code and server code

--- src/host/app.js
+++ src/host/app.js
@@ -3,99 +3,67 @@ const path = require('path');
 const url = require('url');
 var os = require('os');
 
-const interfaces = os.networkInterfaces();
-const addresses = [];
-for (let k in interfaces) {
-    for (let k2 in interfaces[k]) {
-        const address = interfaces[k][k2];
-        if (address.family === 'IPv4' && !address.internal) {
-            addresses.push(address.address);
+class App {
+  constructor () {
+    this.win = null;
+  }
+
+  init () {
+    const interfaces = os.networkInterfaces();
+    const addresses = [];
+    for (let k in interfaces) {
+        for (let k2 in interfaces[k]) {
+            const address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal) {
+                addresses.push(address.address);
+            }
         }
     }
+
+    console.log(addresses);
+
+    app.on('ready', this.createWindow);
+
+    // Quit when all windows are closed.
+    app.on('window-all-closed', () => {
+      // On macOS it is common for applications and their menu bar
+      // to stay active until the user quits explicitly with Cmd + Q
+      if (process.platform !== 'darwin') {
+        app.quit();
+      }
+    });
+
+    app.on('activate', () => {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (this.win === null) {
+        this.createWindow();
+      }
+    });
+  }
+
+  createWindow () {
+    // Create the browser window.
+    this.win = new BrowserWindow({width: 1000, height: 600});
+
+    // and load the index.html of the app.
+    this.win.loadURL(url.format({
+      pathname: path.join(__dirname, '../../build/host.html'),
+      protocol: 'file:',
+      slashes: true,
+    }));
+
+    // Open the DevTools.
+    this.win.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    this.win.on('closed', () => {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      this.win = null;
+    });
+  }
 }
 
-console.log(addresses);
-
-const express = require('express');
-const server = express();
-const http = require('http').Server(server);
-const io = require('socket.io')(http);
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win;
-
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});
-
-  // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, '../../build/host.html'),
-    protocol: 'file:',
-    slashes: true,
-  }));
-
-  // Open the DevTools.
-  win.webContents.openDevTools();
-
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
-}
-
-// When Electron has finished initializing, start the http server.
-server.use(express.static('build'));
-
-server.get('/', (req, res) => {
-  const chatPath = path.join(__dirname, '../../build/client.html');
-  res.sendFile(chatPath);
-});
-
-io.on('connection', socket => {
-  console.log('a user connected');
-
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    console.log('message: ' + msg);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
-
-http.listen(3000, () => {
-  console.log('listening on *:3000');
-
-  app.on('ready', createWindow);
-
-  // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
-  });
-
-  app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-      createWindow();
-    }
-  });
-});
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+module.exports = new App();
