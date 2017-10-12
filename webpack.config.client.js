e02fcb94d67ab008a16b30a46ee71f5eab3199f6
Robbie Antenesse <dev@alamantus.com>
2017-10-11 20:12:27 -0600

    DRY out the webpack configs

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -2,142 +2,37 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const BUILD_DIR = path.resolve(__dirname, 'build');
-const APP_DIR = path.resolve(__dirname, 'src/client');
+const webpackExport = require('./webpack.config.base.js');
 
-const webpackExport = {
-  entry: APP_DIR + '/index.js',
-  output: {
-    path: BUILD_DIR,
-    filename: 'local-rpg.js',
+webpackExport.entry = path.resolve(__dirname, 'src/client', 'index.js');
+webpackExport.output.filename = 'local-rpg.js';
+
+webpackExport.plugins.push(new webpack.DefinePlugin({
+  BASEPATH: JSON.stringify(''),
+}));
+webpackExport.plugins.push(new webpack.optimize.UglifyJsPlugin({
+  compress: {
+    warnings: false,
   },
-
-  module: {
-    rules: [
-      {
-        // Compile ES6 and JSX into regular JavaScript
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              "presets": [
-                'es2015',
-                'es2016',
-              ],
-            },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      // {
-      //   test: /\.(html?)$/,
-      //   use: [
-      //     {
-      //       loader: 'file-loader',
-      //       options: {
-      //         name: '[name].[ext]'
-      //       },
-      //     },
-      //   ],
-      // },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'assets/fonts/[name].[ext]?[hash]'
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(jpe?g|png|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'assets/images/[name].[ext]?[hash]'
-            },
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-              },
-              optipng: {
-                optimizationLevel: 7,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              pngquant:{
-                quality: "65-90",
-                speed: 4,
-              },
-            },
-          },
-        ],
-      },
-    ],
+}));
+webpackExport.plugins.push(new HtmlWebpackPlugin({
+  template: 'src/client/index.html',
+  filename: 'client.html',
+  minify: {
+    removeComments: true,
+    collapseWhitespace: true,
+    removeRedundantAttributes: true,
+    useShortDoctype: true,
+    removeEmptyAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    keepClosingSlash: true,
+    minifyJS: true,
+    minifyCSS: true,
+    minifyURLs: true,
   },
+  inject: true,
+}));
 
-  node: {
-    __dirname: false,
-    __filename: false
-  },
-
-  resolve: {
-    // Make sure Webpack knows that .jsx is a thing to watch out for.
-    extensions: [
-      '.js',
-      '.json',
-    ],
-  },
-
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-    }),
-    new webpack.DefinePlugin({
-      BASEPATH: JSON.stringify(''),
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-    }),
-    new HtmlWebpackPlugin({
-      template: 'src/client/index.html',
-      filename: 'client.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-      },
-      inject: true,
-    }),
-  ],
-
-  devtool: 'source-map'
-};
+webpackExport.devtool = 'source-map';
 
 module.exports = webpackExport;
