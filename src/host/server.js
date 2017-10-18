3fff4edf891f83abdc407cbcd1dccd68c8c136fe
Robbie Antenesse <dev@alamantus.com>
2017-10-18 12:51:46 -0600

    Move server.js to class to call from App.
    Gives more control of when the server is actually started after the app
    opens. This will make it possible to have the DM create session
    settings and specify other data before serving it to players.

--- src/host/server.js
+++ src/host/server.js
@@ -1,56 +1,87 @@
 const path = require('path');
 const express = require('express');
-const server = express();
-const http = require('http').Server(server);
-const io = require('socket.io')(http);
-var os = require('os');
+const os = require('os');
+const http = require('http');
+const socketio = require('socket.io');
 
-const app = require('./app');
+class Server {
+  constructor () {
+    this.server = express();
+    this.http = http.Server(this.server);
+    this.io = socketio(this.http);
+    this.ips = this.getIPs();
 
-const interfaces = os.networkInterfaces();
-const addresses = [];
-for (let k in interfaces) {
-    for (let k2 in interfaces[k]) {
+    this.hasStarted = false;
+
+    this.setupExpress();
+    this.setupSocketIO();
+  }
+
+  getIPs () {
+    const interfaces = os.networkInterfaces();
+    const addresses = [];
+    for (let k in interfaces) {
+      for (let k2 in interfaces[k]) {
         const address = interfaces[k][k2];
         if (address.family === 'IPv4' && !address.internal) {
-            addresses.push(address.address);
+          addresses.push(address.address);
         }
+      }
     }
+    return addresses;
+  }
+
+  setupExpress () {
+    // When Electron has finished initializing, start the http server.
+    this.server.use(express.static(path.join(__dirname, '../../build/client/')));
+
+    this.server.get('/', (req, res) => {
+      const chatPath = path.join(__dirname, '../../build/client/client.html');
+      res.sendFile(chatPath);
+    });
+  }
+
+  setupSocketIO () {
+    this.io.on('connection', socket => {
+      console.log('a user connected');
+      this.emit('console.log', this.ips);
+
+      socket.on('chat message', function(msg){
+        this.emit('chat message', msg);
+        console.log('message: ' + msg);
+      });
+
+      socket.on('moved piece', function(pieceData){
+        this.emit('moved piece', pieceData);
+        console.log(`movedPiece:\n\tid: ${pieceData.id}\n\ttop: ${pieceData.position.top}, left: ${pieceData.position.left}`);
+      });
+
+      socket.on('roll die', function(rollData){
+        this.emit('roll die', rollData);
+      });
+
+      socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+    });
+  }
+
+  emit (triggerString, value) {
+    // Arbitrarily emit a value to all connected sockets
+    // Soon, a second emitTo() method will be added to emit to specific sockets.
+    this.io.emit(triggerString, value);
+  }
+
+  start (onStart = () => {}) {
+    if (!this.hasStarted) {
+      this.http.listen(3000, () => {
+        this.hasStarted = true;
+        onStart();
+      });
+    } else {
+      console.log('The server has already been started!');
+    }
+  }
 }
 
-// When Electron has finished initializing, start the http server.
-server.use(express.static(path.join(__dirname, '../../build/client/')));
-
-server.get('/', (req, res) => {
-  const chatPath = path.join(__dirname, '../../build/client/client.html');
-  res.sendFile(chatPath);
-});
-
-io.on('connection', socket => {
-  console.log('a user connected');
-  io.emit('console.log', addresses);
-
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    console.log('message: ' + msg);
-  });
-
-  socket.on('moved piece', function(pieceData){
-    io.emit('moved piece', pieceData);
-    console.log(`movedPiece:\n\tid: ${pieceData.id}\n\ttop: ${pieceData.position.top}, left: ${pieceData.position.left}`);
-  });
-
-  socket.on('roll die', function(rollData){
-    io.emit('roll die', rollData);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
-
-http.listen(3000, () => {
-  console.log(`Connect on ${addresses[(addresses.length - 1)]}:3000`);
-
-  app.init();
-});
+module.exports = new Server();
