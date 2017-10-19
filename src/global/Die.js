feeeacb15c64523ca3994a64e9c1758c622bdd6c
Robbie Antenesse <dev@alamantus.com>
2017-10-18 18:09:05 -0600

    Update Die code formatting

--- src/global/Die.js
+++ src/global/Die.js
@@ -1,17 +1,17 @@
 export class Die {
-  constructor (faces = [1,2,3,4,5,6]) {
-    this.faces = faces
-    this.numberSides = faces.length
-    this.sideShowing = faces[0]
+  constructor (faces = [1, 2, 3, 4, 5, 6]) {
+    this.faces = faces;
+    this.numberSides = faces.length;
+    this.sideShowing = faces[0];
   }
 
   rollSelf () {
-    var sideNumber = Math.floor(Math.random() * this.numberSides)
-    this.sideShowing = this.faces[sideNumber]
-    return this.sideShowing
+    var sideNumber = Math.floor(Math.random() * this.numberSides);
+    this.sideShowing = this.faces[sideNumber];
+    return this.sideShowing;
   }
 
   checkSideShowing () {
-    return this.sideShowing
+    return this.sideShowing;
   }
 }
