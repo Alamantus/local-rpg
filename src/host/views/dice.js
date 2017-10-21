f3ca0898cfd5dbd4f7fbe09000f3c53af95cf577
Robbie Antenesse <dev@alamantus.com>
2017-10-21 01:06:08 -0600

    Create dice view stub

---
+++ src/host/views/dice.js
@@ -0,0 +1,60 @@
+import html from 'choo/html';
+
+// import { CharactersController } from ‘../controllers/CharactersController’;
+
+export default (state, emit) => {
+  // const controller = new CharactersController(state);
+
+  return html`<div>
+  <div class="columns is-multiline">
+    <div class="column is-one-quarter">
+      <div class="card">
+        <div class="card-content">
+          <h3 class="title is-3">Custom Die</h3>
+        </div>
+        <footer class="card-footer">
+          <a href="#" class="card-footer-item">View</a>
+          <a href="#" class="card-footer-item">Edit</a>
+          <a href="#" class="card-footer-item">Delete</a>
+        </footer>
+      </div>
+    </div>
+    <div class="column is-one-quarter">
+      <div class="card">
+        <div class="card-content">
+          <h3 class="title is-3">Custom Die</h3>
+        </div>
+        <footer class="card-footer">
+          <a href="#" class="card-footer-item">View</a>
+          <a href="#" class="card-footer-item">Edit</a>
+          <a href="#" class="card-footer-item">Delete</a>
+        </footer>
+      </div>
+    </div>
+    <div class="column is-one-quarter">
+      <div class="card">
+        <div class="card-content">
+          <h3 class="title is-3">Custom Die</h3>
+        </div>
+        <footer class="card-footer">
+          <a href="#" class="card-footer-item">View</a>
+          <a href="#" class="card-footer-item">Edit</a>
+          <a href="#" class="card-footer-item">Delete</a>
+        </footer>
+      </div>
+    </div>
+    <div class="column is-one-quarter">
+      <div class="card">
+        <div class="card-content">
+          <a onclick=${() => {
+            emit('change view', 'character creation')}
+          }>
+            <h3 class="title is-3">Add New Sheet</h3>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  </div>`;
+}
