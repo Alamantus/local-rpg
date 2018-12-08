bb832d2ff2febf77e2a98ace9d0191c12c979a42
Robbie Antenesse <dev@alamantus.com>
2018-12-07 17:04:00 -0700

    Skeleton in character control; Make scratchNotes update oninput

--- src/host/views/table/index.js
+++ src/host/views/table/index.js
@@ -36,14 +36,59 @@ export default (state, emit) => {
       <div class="column is-one-third">
         <article class="box">
           <h2 class="title">Scratch Notes</h2>
-          <textarea id="scratch" class="textarea" onchange=${event => controller.state.scratchNotes = event.target.value}>${controller.state.scratchNotes}</textarea>
+          <textarea id="scratch" class="textarea" oninput=${event => controller.state.scratchNotes = event.target.value}>${controller.state.scratchNotes}</textarea>
         </article>
       </div>
     </div>`,
     html`<div class="columns">
       <div class="column">
         <article class="box">
-          <h2 class="title">Whispers</h2>
+          <h2 class="title">Characters</h2>
+          <div class="columns">
+            <div class="column is-one-quarter">
+              <nav class="panel">
+                <div class="panel-block">
+                  <p class="control has-icons-left">
+                    <input class="input is-small" type="text" placeholder="search">
+                    <span class="icon is-small is-left">
+                      <i class="fa fa-search"></i>
+                    </span>
+                  </p>
+                </div>
+                <h4 class="panel-heading">
+                  Player Characters
+                </h4>
+                <a class="panel-block is-active">
+                  Some Player
+                </a>
+                <a class="panel-block">
+                  Some Other Player
+                </a>
+
+                <h4 class="panel-heading">
+                  Non-Player Characters
+                  <a class="button is-success is-small is-pulled-right"><span class="icon"><i class="fa fa-plus"></i></span></a>
+                </h4>
+                <div class="panel-block">
+                  
+                </div>
+              </nav>
+            </div>
+            <div class="column is-three-quarters">
+              <h3 class="title is-3">
+                Some Player
+              </h2>
+              <a class="button">
+                Whisper
+              </a>
+              <a class="button">
+                Character Sheet
+              </a>
+              <a class="button">
+                Manage Items
+              </a>
+            </div>
+          </div>
         </article>
       </div>
     </div>`,
